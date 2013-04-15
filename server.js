4c0cf3a5faed2cee483d82006ff1a320fd238bc1
Nima Rahnemoon <nrahnemoon@gmail.com>
2013-04-15 02:58:11 -0700

    Reduced buffer time and fixed node server to provide static files.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var peerWaiting = false;
 var waitingPeer = "";
 var res1;
 
-connect.createServer(connect.static("bin-release")).listen(55555);
+connect().use(connect.static(__dirname + "/bin-release")).listen(55555);
 
 http.createServer(function(request, response) {
   console.log('New request');
