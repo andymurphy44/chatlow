62aac371ade907d0a37388a0c0f8ec8e532d3b46
Nima Rahnemoon <nrahnemoon@gmail.com>
2013-04-14 18:28:29 -0700

    Working project.  Really.

---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require("http");
+var url = require("url");
+var connect = require("connect");
+var peerWaiting = false;
+var waitingPeer = "";
+var res1;
+
+connect.createServer(connect.static("bin-debug")).listen(8080);
+
+http.createServer(function(request, response) {
+  console.log('New request');
+  console.log("Request URL = " + request.url);
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  var queryData = url.parse(request.url, true).query;
+  console.log('findPeer request : ' + JSON.stringify(queryData));
+  if (peerWaiting) {
+    response.write("<id>" + waitingPeer + "</id>");
+    res1.write("<id>" + queryData.id + "</id>");
+    res1.end();
+    response.end();
+    peerWaiting = false;
+  } else {
+    waitingPeer = queryData.id;
+    res1 = response;
+    peerWaiting = true;
+  }
+}).listen(8888);
