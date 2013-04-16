3566f54a1dcb0b0a85c1b80fdbc82cb46f1b6f39
Nima Rahnemoon <nrahnemoon@gmail.com>
2013-04-15 19:00:52 -0700

    Server wasn't "lockable" so multiple requests one after another caused race conditions.  Fixed.

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
 var express = require('express');
 var url = require('url');
 var app = express();
-var peerWaiting = false;
-var waitingPeer = "";
-var res1;
+var waitingPeers = [];
 
 app.configure(function() {
   app.use(express.static(__dirname + "/bin-release"));
 });
 
 app.get('/findPeer', function(request, response) {
-  console.log('New request');
   console.log("Request URL = " + request.url);
   response.writeHead(200, {"Content-Type": "text/plain"});
   var queryData = url.parse(request.url, true).query;
-  console.log('findPeer request : ' + JSON.stringify(queryData));
+  console.log('findPeer request : ' + JSON.stringify(queryData) + '\n');
   if (queryData.id && queryData.id != "") {
-    if (peerWaiting) {
-      response.write("<id>" + waitingPeer + "</id>");
+    if (waitingPeers.length != 0) {
+      var peer = waitingPeers.pop();
       if (queryData.id.indexOf("/") != -1) {
         queryData.id = queryData.id.substr(0, queryData.id.indexOf('/'));
       }
-      res1.write("<id>" + queryData.id + "</id>");
-      res1.end();
-      response.end();
-      peerWaiting = false;
+      peer[1].end("<id>" + queryData.id + "</id>");
+      response.end("<id>" +  peer[0] + "</id>");
     } else {
-      waitingPeer = queryData.id;
-      if (waitingPeer.indexOf("/") != -1) {
-        waitingPeer = waitingPeer.substr(0, waitingPeer.indexOf('/'));
+      if (queryData.id.indexOf("/") != -1) {
+        queryData.id = queryData.id.substr(0, queryData.id.indexOf('/'));
       }
-      res1 = response;
-      peerWaiting = true;
+      waitingPeers.push([queryData.id, response]);
     }
   }
 });
